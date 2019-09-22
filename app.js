4395c8fd4914def6db3ab28a6d6f52b39d9d2742
shawnmian <51836930+shawnmian@users.noreply.github.com>
2019-09-21 19:59:06 -0700

    Circle Overlay
    Added circle drawing overlay ontop of video feed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video 
-    imageScaleFactor: 0.5,  // reduce input image size for gains in speed.
+    imageScaleFactor: 0.2,  // reduce input image size for gains in speed.
     maxNumBoxes: 20,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
     scoreThreshold: 0.79,    // confidence threshold for predictions.
@@ -21,23 +21,20 @@ const context = canvas.getContext('2d');
 let model;
 
 
-handTrack.startVideo(video).then(status => {
-    if(status){
-        navigator.getUserMedia(
-            {video:{}},
-              stream => {
-                video.srcObject = stream;
-                setInterval(runDetection,140);
-            },
-            err => console.log(err)
-        );
-    }
-});
+
 
 function runDetection(){
     model.detect(video)
         .then(predictions => {
-            console.log(predictions);
+            // console.log(predictions);
+            // console.log(model.getModelParameters());
+            if (predictions[0]) {
+                let x = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2);
+                let y = predictions[0].bbox[1] + (predictions[0].bbox[3] / 2);
+
+                console.log(x,y);
+                animateCircles(x, y);
+            }
             model.renderPredictions(predictions, canvas, context, video,);
         });
 }
@@ -46,3 +43,56 @@ function runDetection(){
 handTrack.load(modelParams).then(lmodel => {
         model = lmodel;
     })
+
+
+
+    
+    // document.addEventListener("mousemove", animateCircles);
+
+    var colors = ['#C4FAF8', '#FFABAB', '#D5AAFF']
+           
+   function animateCircles (x,y)
+    {
+       var circle = document.createElement("div");
+       circle.setAttribute("id", "mainDiv");
+       circle.setAttribute("class", "circle");
+   
+       document.body.appendChild(circle);
+
+       circle.style.left = x + 'px';
+       circle.style.top = y + 'px';
+       
+       var color = colors[Math.floor(Math.random() * colors.length)];
+       circle.style.borderColor = color;
+
+
+
+       circle.style.transition = "all 0.5s linear 0s";
+
+       circle.style.left = circle.offsetLeft - 20 - 'px';
+       circle.style.top =circle.offsetRight -20 + 'px';
+
+       circle.style.width = "100px";
+       circle.style.height = "100px";
+       circle.style.borderLength = "50px";
+       circle.style.opacity = 0;
+       
+
+       // setTimeout(function(){document.body.removeChild(circle)}, 1000);
+   // 
+    }
+    
+    
+    handTrack.startVideo(video).then(status => {
+    if(status){
+        navigator.getUserMedia(
+            {video:{}},
+              stream => {
+                video.srcObject = stream;
+                setInterval(runDetection,10);
+            },
+            err => console.log(err)
+        );
+    }
+});
+
